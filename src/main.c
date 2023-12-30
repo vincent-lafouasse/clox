4856a5462fae38e07bf06a0c9684d5b0eb81a755
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-30 16:49:41 +0100

    disassemble chunk

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "chunk.h"
+#include "debug.h"
 #include "common.h"
 
 int main(void)
@@ -6,6 +7,7 @@ int main(void)
     Chunk chunk;
     initChunk(&chunk);
     writeChunk(&chunk, OP_RETURN);
+	disassembleChunk(&chunk, "a nice chunk");
     freeChunk(&chunk);
     return 0;
 }
